041ed3963b9d62890dbf5e8e763eef1249870a09
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-07-17 11:03:16 +0200

    Update dlib_face_detect.py (#8516)

--- homeassistant/components/image_processing/dlib_face_detect.py
+++ homeassistant/components/image_processing/dlib_face_detect.py
@@ -19,8 +19,6 @@ REQUIREMENTS = ['face_recognition==0.2.0']
 
 _LOGGER = logging.getLogger(__name__)
 
-ATTR_LOCATION = 'location'
-
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the Dlib Face detection platform."""
@@ -70,7 +68,4 @@ class DlibFaceDetectEntity(ImageProcessingFaceEntity):
         image = face_recognition.load_image_file(fak_file)
         face_locations = face_recognition.face_locations(image)
 
-        face_locations = [{ATTR_LOCATION: location}
-                          for location in face_locations]
-
-        self.process_faces(face_locations, len(face_locations))
+        self.process_faces(list(face_locations), len(face_locations))
